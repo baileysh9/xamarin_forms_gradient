c69eb081c84d1da13b363b6c9efa3d255855db93
Scott Bailey <sbailey@brownbagmarketing.com>
2017-07-14 13:11:52 -0400

    first commit

---
+++ GradientTest/Droid/GradientViewRenderer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Linq;
+using Android.App;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.Views;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: Xamarin.Forms.ExportRenderer(typeof(GradientTest.GradientViewRender), typeof(GradientTest.Droid.GradientViewRenderer))]
+namespace GradientTest.Droid
+{
+	public class GradientViewRenderer : Xamarin.Forms.Platform.Android.ViewRenderer<GradientTest.GradientViewRender, View>
+	{
+		LinearLayout layout;
+        Xamarin.Forms.Color[] gradientColors;
+        float cornerRadius;
+		double viewHeight;
+		double viewWidth;
+		bool roundCorners;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<GradientTest.GradientViewRender> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control == null)
+			{
+				layout = new LinearLayout(Application.Context);
+				layout.SetBackgroundColor(Color.White);
+			
+                gradientColors = (Xamarin.Forms.Color[])e.NewElement.GradientColors;
+				cornerRadius = (float)e.NewElement.CornerRadius;
+				viewWidth = (double)e.NewElement.ViewWidth;
+				viewHeight = (double)e.NewElement.ViewHeight;
+				roundCorners = (bool)e.NewElement.RoundCorners;
+
+				CreateLayout();
+			}
+
+			if (e.OldElement != null)
+			{
+				// Unsubscribe from event handlers and cleanup any resources
+			}
+
+			if (e.NewElement != null)
+			{
+				// Configure the control and subscribe to event handlers
+				gradientColors = (Xamarin.Forms.Color[])e.NewElement.GradientColors;
+				cornerRadius = (float)e.NewElement.CornerRadius;
+				viewWidth = (double)e.NewElement.ViewWidth;
+				viewHeight = (double)e.NewElement.ViewHeight;
+				roundCorners = (bool)e.NewElement.RoundCorners;
+
+				CreateLayout();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == GradientViewRender.ViewHeightProperty.PropertyName)
+			{
+				this.viewHeight = (double)this.Element.ViewHeight;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.ViewWidthProperty.PropertyName)
+			{
+				this.viewWidth = (double)this.Element.ViewWidth;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.GradientColorsProperty.PropertyName)
+			{
+                this.gradientColors = (Xamarin.Forms.Color[])this.Element.GradientColors;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.CornerRadiusProperty.PropertyName)
+			{
+				this.cornerRadius = (float)this.Element.CornerRadius;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.RoundCornersProperty.PropertyName)
+			{
+				this.roundCorners = (bool)this.Element.RoundCorners;
+				CreateLayout();
+			}
+		}
+
+		private void CreateLayout()
+		{
+			layout.SetMinimumWidth((int)viewWidth);
+			layout.SetMinimumHeight((int)viewHeight);
+
+			CreateGradient();
+
+            SetNativeControl(layout);
+		}
+
+		public void CreateGradient()
+		{
+			//Need to convert the colors to Android Color objects
+			int[] androidColors = new int[gradientColors.Count()];
+
+			for (int i = 0; i < gradientColors.Count(); i++)
+			{
+                Xamarin.Forms.Color temp = gradientColors[i];
+                androidColors[i] = temp.ToAndroid();
+			}
+
+			GradientDrawable gradient = new GradientDrawable(GradientDrawable.Orientation.LeftRight, androidColors);
+
+			if (roundCorners)
+                gradient.SetCornerRadii(new float[] { cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius, cornerRadius });
+
+			layout.SetBackground(gradient);
+		}
+	}
+}
---
+++ GradientTest/iOS/GradientViewRenderer.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using CoreAnimation;
+using CoreGraphics;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using System.Linq;
+
+[assembly: ExportRenderer(typeof(GradientTest.GradientViewRender), typeof(GradientTest.iOS.GradientViewRenderer))]
+namespace GradientTest.iOS
+{
+	public class GradientViewRenderer : ViewRenderer<GradientTest.GradientViewRender, UIView>
+	{
+		UIView mainView;
+		UIView gradientView;
+        Color[] gradientColors;
+		double cornerRadius;
+		double viewHeight;
+		double viewWidth;
+		bool roundCorners;
+		bool leftToRight;
+
+		protected override void OnElementChanged(ElementChangedEventArgs<GradientTest.GradientViewRender> e)
+		{
+			base.OnElementChanged(e);
+
+			if (Control == null)
+			{
+                gradientColors = (Color[])e.NewElement.GradientColors;
+				cornerRadius = (double)e.NewElement.CornerRadius;
+				viewWidth = (double)e.NewElement.ViewWidth;
+				viewHeight = (double)e.NewElement.ViewHeight;
+				roundCorners = (bool)e.NewElement.RoundCorners;
+				leftToRight = (bool)e.NewElement.LeftToRight;
+
+				mainView = new UIView(new CGRect(0, 0, viewWidth, viewHeight));
+				//contentView = new UIView(new CGRect(0, 0, viewWidth, viewHeight));
+				gradientView = new UIView(new CGRect(0, 0, viewWidth, viewHeight));
+
+				CreateLayout();
+			}
+
+			if (e.OldElement != null)
+			{
+				// Unsubscribe from event handlers and cleanup any resources
+			}
+
+			if (e.NewElement != null)
+			{
+				gradientColors = (Color[])e.NewElement.GradientColors;
+				cornerRadius = (double)e.NewElement.CornerRadius;
+				viewWidth = (double)e.NewElement.ViewWidth;
+				viewHeight = (double)e.NewElement.ViewHeight;
+				roundCorners = (bool)e.NewElement.RoundCorners;
+				leftToRight = (bool)e.NewElement.LeftToRight;
+
+				CreateLayout();
+			}
+		}
+
+		protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			base.OnElementPropertyChanged(sender, e);
+
+			if (e.PropertyName == GradientViewRender.ViewHeightProperty.PropertyName)
+			{
+				this.viewHeight = (double)this.Element.ViewHeight;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.ViewWidthProperty.PropertyName)
+			{
+				this.viewWidth = (double)this.Element.ViewWidth;
+				CreateLayout();
+			}
+            else if (e.PropertyName == GradientViewRender.GradientColorsProperty.PropertyName)
+			{
+                this.gradientColors = (Color[])this.Element.GradientColors;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.CornerRadiusProperty.PropertyName)
+			{
+				this.cornerRadius = (double)this.Element.CornerRadius;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.RoundCornersProperty.PropertyName)
+			{
+				this.roundCorners = (bool)this.Element.RoundCorners;
+				CreateLayout();
+			}
+			else if (e.PropertyName == GradientViewRender.LeftToRightProperty.PropertyName)
+			{
+				this.leftToRight = (bool)this.Element.LeftToRight;
+				CreateLayout();
+			}
+		}
+
+		private void CreateLayout()
+		{
+            //Clean up any previous views
+			if (Control != null)
+			{
+				foreach (UIView view in Control)
+				{
+					view.RemoveFromSuperview();
+				}
+			}
+
+			mainView = new UIView(new CGRect(0, 0, viewWidth, viewHeight));
+			gradientView = new UIView(new CGRect(0, 0, viewWidth, viewHeight));
+
+			mainView.BackgroundColor = UIColor.Clear;
+
+			CreateGradient();
+            mainView.ClipsToBounds = true;
+			gradientView.Layer.MasksToBounds = false;
+		
+			mainView.AddSubview(gradientView);
+
+			SetNativeControl(mainView);
+		}
+
+		public void CreateGradient()
+		{
+			CAGradientLayer gradient = new CAGradientLayer();
+
+			CGRect tempRect = new CGRect(0, 0, this.viewWidth, this.viewHeight);
+			gradient.Frame = tempRect;
+
+            //Need to convert the colors to CGColor objects
+            CGColor[] cgColors = new CGColor[gradientColors.Count()];
+
+            for (int i = 0; i < gradientColors.Count(); i++)
+            {
+                Color temp = gradientColors[i];
+                cgColors[i] = temp.ToCGColor();
+            }
+
+            gradient.Colors = cgColors;
+
+			//Determine if the gradient should be vertical or left to right
+			if (leftToRight)
+			{
+				gradient.StartPoint = new CGPoint(0, 0.5);
+				gradient.EndPoint = new CGPoint(1, 0.5);
+			}
+			else
+			{
+				gradient.StartPoint = new CGPoint(0.5, 0);
+				gradient.EndPoint = new CGPoint(0.5, 1);
+			}
+
+			CGRect rec = new CGRect(0, 0, viewWidth, viewHeight);
+
+			var rounded = UIRectCorner.TopLeft | UIRectCorner.TopRight | UIRectCorner.BottomLeft | UIRectCorner.BottomRight;
+
+            if (roundCorners)
+            {
+				UIBezierPath path = UIBezierPath.FromRoundedRect(rec, rounded, new CGSize(cornerRadius, cornerRadius));
+
+				CAShapeLayer shape = new CAShapeLayer();
+				shape.Path = path.CGPath;
+
+				gradientView.Layer.Mask = shape;
+            }
+
+            gradientView.Layer.InsertSublayer(gradient, 0);
+		}
+	}
+}
